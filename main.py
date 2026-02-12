cb6ef2bf587300c1aca87a3238432e773b7b0157
ubobur2197 <ubobur2197@gmail.com>
2026-02-12 15:35:57 +0500

    Add examples of string methods in main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# 1
+text = " "
+print(text.isspace())
+
+
+# 2
+text = "SALOM"
+print(text.isupper())
+
+
+# 3
+text = "salom"
+print(text.islower())
+
+
+# 4
+text = "Python"
+print(text.center(30))
+
+
+# 5
+text = "Python"
+print(text.ljust(25))
